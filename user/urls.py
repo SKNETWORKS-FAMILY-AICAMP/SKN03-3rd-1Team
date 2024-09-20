f9b34f7819ece9709d3174acebad509e767f08f5
Lerini98 <hyerin7259@gmail.com>
2024-09-20 23:26:20 +0900

    web design update

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 # app.url.py -> request & view를 연결해줌
 urlpatterns = [
     # localhost
-    path('Email/', views.Profile, name='Email'),
+    path('Massage/', views.Profile, name='Massage'),
     path('Coupon/', views.Profile, name='Coupon'),
     path('', views.Profile, name='Profile'),
     path('Profile/', views.Profile, name='Profile')
